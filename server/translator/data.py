62005fa44c0ec56f3e16062c698edc5b5760e1da
David P <35774964+DavidO256@users.noreply.github.com>
2019-07-23 09:37:08 -0400

    Various bug fixes.

--- server/translator/data.py
+++ server/translator/data.py
@@ -1,5 +1,7 @@
+import tensorflow as tf
 import numpy as np
 import nltk
+import math
 
 def make_vocabulary(corpus, case_sensitive, vocabulary_cutoff):
     occurrences = dict()
@@ -16,7 +18,8 @@ def process_inputs(inputs, vocabulary, settings):
     result = list()
     for token in nltk.wordpunct_tokenize(inputs):
         result.append(0 if token not in vocabulary else 1 + vocabulary.index(token))
-    return np.asarray(result + [0] * (settings['encoder_inputs'] - len(result)))
+    return np.asarray(result[:settings['encoder_inputs']]
+                      + [0] * (settings['encoder_inputs'] - len(result)))
 
 def process_outputs(outputs, vocabulary, settings):
     result = list()
@@ -25,6 +28,8 @@ def process_outputs(outputs, vocabulary, settings):
         if token in vocabulary:
             y[vocabulary.index(token)] = 1
         result.append(y)
+    return np.asarray(result[:settings['encoder_inputs']]
+                      + [np.zeros(len(vocabulary))] * (settings['encoder_inputs'] - len(result)))
 
 def parse_outputs(outputs, vocabulary, settings):
     result = list()
@@ -38,3 +43,27 @@ def load_corpus(path):
         for sample in f:
             corpus.append(sample.replace("\n", str()))
     return corpus
+
+
+class Dataset(tf.keras.utils.Sequence):
+
+    def __init__(self, x_corpus, y_corpus, x_vocab, y_vocab, settings):
+        self.x_corpus = x_corpus
+        self.y_corpus = y_corpus
+        self.x_vocab = x_vocab
+        self.y_vocab = y_vocab
+        self.settings = settings
+
+    def __len__(self):
+        return int(math.ceil(len(self.x_corpus) // self.settings['batch_size']))
+
+    def __getitem__(self, index):
+        start = index * self.settings['batch_size']
+        end = (index + 1) * self.settings['batch_size']
+        x = [process_inputs(sample, self.x_vocab, self.settings) for sample in self.x_corpus[start:end]]
+        y = [process_inputs(sample, self.y_vocab, self.settings) for sample in self.y_corpus[start:end]]
+        for n in range(len(x)):
+            if len(x[n]) == 0 or len(y[n]) == 0:
+                x.pop(n)
+                y.pop(n)
+        return np.asarray(x), np.asarray(y)
--- server/translator/model.py
+++ server/translator/model.py
@@ -1,24 +1,21 @@
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
 import tensorflow as tf
 import translator.data
 import numpy as np
 
 
 def create_model(settings, vocabulary_size):
-    inputs = tf.keras.layers.Input(settings['encoder_inputs'], name="encoder_inputs")
-    embedding = tf.keras.layers.Embedding(vocabulary_size, settings['embedding_size'])(inputs)
-    encoder = tf.keras.layers.RNN([tf.keras.layers.LSTMCell(settings['units'], name="encoder_cell_%d" % n)
-                                   for n in range(settings['encoder_cells'])], name="encoder",
-                                   return_sequences=True)(embedding)
-
-    score = tf.keras.layers.Dense(settings['embedding_size'], tf.keras.activations.tanh)
-
-    decoder = tf.keras.layers.RNN([tf.keras.layers.LSTMCell(settings['units'], name="decoder_cell_%d" % n)
-                                   for n in range(settings['decoder_cells'])], name="decoder",
-                                   return_sequences=True)(encoder)
+    inputs = tf.keras.layers.Input((settings['encoder_inputs'], ), name="encoder_inputs")
+    embedding = tf.keras.layers.Embedding(vocabulary_size + 1, settings['embedding_size'])(inputs)
+    encoder = tf.keras.layers.GRU(settings['units'], name="encoder", return_sequences=True)(embedding)
+    decoder = tf.keras.layers.GRU(settings['units'], name='decoder', return_sequences=True)(encoder)
     decoder_attention = tf.keras.layers.Attention(name="decoder_attention")([encoder, decoder])
-    decoder_outputs = tf.keras.layers.TimeDistributed(tf.keras.layers.Dense(vocabulary_size, name="outputs"))(decoder_attention)
-    outputs = tf.keras.layers.Softmax()(decoder_outputs)
-
+    outputs = tf.keras.layers.Dense(vocabulary_size, activation='softmax', name="outputs")(decoder_attention)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     model.compile(settings['optimizer'], settings['loss'], settings['metrics'])
     return model
@@ -27,25 +24,33 @@ def create_model(settings, vocabulary_size):
 if __name__ == '__main__':
     config = {
             "encoder_inputs": 64,
-            "embedding_size": 512,
+            "embedding_size": 256,
             "units": 128,
             "encoder_cells": 3,
             "decoder_cells": 3,
-            "optimizer": "adam",
-            "loss": "mse",
+            "optimizer": "rmsprop",
+            "loss": "sparse_categorical_crossentropy",
+            "batch_size": 32,
+            "epochs": 3,
             "metrics": [
-                "accuracy"
             ]
         }
-    m = create_model(config, 50)
-    tf.keras.utils.plot_model(m, "model.png", show_shapes=True)
-
-    raw_x = translator.data.load_corpus("../datasets/temp/raw/train_ger_x.txt")
-    raw_y = translator.data.load_corpus("../datasets/temp/raw/train_ger_y.txt")
-    german_vocabulary = translator.data.make_vocabulary(raw_x, False, 50)
-    english_vocabulary = translator.data.make_vocabulary(raw_y, False, 50)
-    print("German Vocab:", len(german_vocabulary), "\nEnglish Vocab:", len(english_vocabulary))
-    x = np.asarray([translator.data.process_inputs(sample, german_vocabulary, config) for sample in raw_x])
-    y = np.asarray([translator.data.process_outputs(sample, english_vocabulary, config) for sample in raw_y])
-    print(x.shape, "\n", y.shape)
-    m.fit(x, y)
+    
+    #tf.keras.utils.plot_model(m, "model.png", show_shapes=True)
+    
+    raw_x = translator.data.load_corpus("../datasets/raw/train_ger_x.txt")
+    raw_y = translator.data.load_corpus("../datasets/raw/train_ger_y.txt")
+    german_vocabulary = translator.data.make_vocabulary(raw_x, False, 50000)
+    english_vocabulary = translator.data.make_vocabulary(raw_y, False, 50000)
+    
+    train_generator = translator.data.Dataset(raw_x, raw_y, german_vocabulary, english_vocabulary, config)
+    test_generator = translator.data.Dataset(translator.data.load_corpus("../datasets/raw/test_ger_x.txt"),
+                                             translator.data.load_corpus("../datasets/raw/test_ger_x.txt"),
+                                             german_vocabulary, english_vocabulary, config)
+    m = create_model(config, 50000)
+    with tf.Session() as session:
+        tf.keras.backend.set_session(session)
+        m.fit_generator(train_generator, validation_data=test_generator, epochs=10, callbacks=[
+            tf.keras.callbacks.ModelCheckpoint("models/weights.{epoch:02d}-{val_loss:.3f}.hdf5", 'val_loss'),
+            tf.keras.callbacks.EarlyStopping('val_loss', 0.001, 2),
+            tf.keras.callbacks.TerminateOnNaN(), tf.keras.callbacks.ReduceLROnPlateau(patience=2)])
