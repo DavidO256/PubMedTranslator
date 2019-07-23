7119a77f2f77fe3d1d3559a999d22e0a97402935
David P <35774964+DavidO256@users.noreply.github.com>
2019-07-23 11:42:22 -0400

    Cleaned up pathing and removed bugs.

--- server/translator/data.py
+++ server/translator/data.py
@@ -14,28 +14,29 @@ def make_vocabulary(corpus, case_sensitive, vocabulary_cutoff):
     return list(sorted(occurrences, key=occurrences.get, reverse=True))[:vocabulary_cutoff]
 
 
-def process_inputs(inputs, vocabulary, settings):
+def process_x(inputs, vocabulary, settings):
     result = list()
     for token in nltk.wordpunct_tokenize(inputs):
         result.append(0 if token not in vocabulary else 1 + vocabulary.index(token))
-    return np.asarray(result[:settings['encoder_inputs']]
-                      + [0] * (settings['encoder_inputs'] - len(result)))
+    return np.pad(result[:settings['encoder_inputs']], (0, settings['encoder_inputs'] - len(result)), mode='constant')
 
-def process_outputs(outputs, vocabulary, settings):
+
+def process_y(outputs, vocabulary, settings):
     result = list()
     for token in nltk.wordpunct_tokenize(outputs):
         y = np.zeros(len(vocabulary))
         if token in vocabulary:
             y[vocabulary.index(token)] = 1
         result.append(y)
-    return np.asarray(result[:settings['encoder_inputs']]
-                      + [np.zeros(len(vocabulary))] * (settings['encoder_inputs'] - len(result)))
+    return np.pad(result[:len(vocabulary)], (0, len(vocabulary) - len(result)), mode='constant')
 
-def parse_outputs(outputs, vocabulary, settings):
+
+def convert_raw(data, vocabulary, settings):
     result = list()
-    for vector in outputs:
+    for vector in data:
         result.append(vocabulary[sum(vector * np.arange(len(vocabulary),))])
     return " ".join(result)
+
 
 def load_corpus(path):
     corpus = list()
@@ -60,8 +61,8 @@ class Dataset(tf.keras.utils.Sequence):
     def __getitem__(self, index):
         start = index * self.settings['batch_size']
         end = (index + 1) * self.settings['batch_size']
-        x = [process_inputs(sample, self.x_vocab, self.settings) for sample in self.x_corpus[start:end]]
-        y = [process_inputs(sample, self.y_vocab, self.settings) for sample in self.y_corpus[start:end]]
+        x = [process_x(sample, self.x_vocab, self.settings) for sample in self.x_corpus[start:end]]
+        y = [process_y(sample, self.y_vocab, self.settings) for sample in self.y_corpus[start:end]]
         for n in range(len(x)):
             if len(x[n]) == 0 or len(y[n]) == 0:
                 x.pop(n)
