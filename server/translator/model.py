0ec98dea5a2f43b1b2687813370d0fa2c2679675
David P <35774964+DavidO256@users.noreply.github.com>
2019-07-25 12:02:08 -0400

    Made corrections to model.

--- server/translator/model.py
+++ server/translator/model.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
 import translator.data
 import numpy as np
-    
+
 
 def create_default_model(settings, inputs_vocabulary_size, outputs_vocabulary_size):
     inputs = tf.keras.layers.Input((settings['encoder_inputs'], ), name="encoder_inputs")
-    flat = tf.keras.layers.Flatten()(inputs)
-    embedding = tf.keras.layers.Embedding(inputs_vocabulary_size + 1, settings['embedding_size'])(flat)
-    encoder = tf.keras.layers.GRU(settings['units'], name="encoder", return_sequences=True)(embedding)
-    decoder = tf.keras.layers.GRU(settings['units'], name='decoder', return_sequences=True)(encoder)
+    embedding = tf.keras.layers.Embedding(inputs_vocabulary_size + 1, settings['embedding_size'])(inputs)
+    encoder = tf.keras.layers.CuDNNGRU(settings['units'], name="encoder", return_sequences=True)(embedding)
+    decoder = tf.keras.layers.CuDNNGRU(settings['units'], name='decoder', return_sequences=True)(encoder)
     decoder_attention = tf.keras.layers.Attention(name="decoder_attention")([encoder, decoder])
     outputs = tf.keras.layers.Dense(outputs_vocabulary_size, activation='softmax', name="vocabulary")(decoder_attention)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
@@ -26,23 +25,26 @@ class TranslationModel:
     def __init__(self, settings):
         self.x_vocabulary = None
         self.y_vocabulary = None
-        self.estimator = None
+        self.model = None
         self.settings = settings
 
-    def fit(self, x_corpus, y_corpus, epochs=1, validation_x=None, validation_y=None):
-        self.x_vocabulary, self.y_vocabulary = (translator.data.make_vocabulary(corpus, self.settings['case_sensitive'], self.settings['vocabulary_cutoff']) for corpus in [x_corpus, y_corpus])
+    def fit(self, x_corpus, y_corpus, epochs=1, validation_x=None, validation_y=None, callbacks=None):
+        self.x_vocabulary, self.y_vocabulary = (translator.data.make_vocabulary(corpus, self.settings) for corpus in [x_corpus, y_corpus])
         if self.x_vocabulary is None or self.y_vocabulary is None:
             raise ValueError("Error fitting model: x and y vocabulary must be initialized before or during fitting.")
-        if self.estimator is None:
-            model = create_default_model(self.settings, len(self.x_vocabulary), len(self.y_vocabulary))
-            tf.keras.utils.plot_model(model, "model.png", show_shapes=True)
-            self.estimator = convert_estimator(model)
-        self.estimator.train(input_fn=lambda:translator.data.dataset_generator(x_corpus, y_corpus, self.x_vocabulary, self.y_vocabulary, self.settings['batch_size'], self.settings['encoder_inputs']), steps=len(x_corpus)//self.settings['batch_size'])
+        if self.model is None:
+            self.model = create_default_model(self.settings, len(self.x_vocabulary), len(self.y_vocabulary))
+            tf.keras.utils.plot_model(self.model, "model.png", show_shapes=True)
+        train_dataset = translator.data.Dataset(x_corpus, y_corpus, self.x_vocabulary, self.y_vocabulary, self.settings)
+        fit_kwargs = { 'epochs': epochs, 'callbacks': callbacks }
+        if validation_x is not None and validation_y is not None:
+            fit_kwargs['validation_data'] = translator.data.Dataset(validation_x, validation_y, self.x_vocabulary, self.y_vocabulary, self.settings)
+        self.model.fit_generator(train_dataset, **fit_kwargs)
             
     def predict(x, **kwargs):
         if isinstance(x, str):
             return self.predict([x], **kwargs)
-        return [self.model.predict(tf.convert_to_tensor(translator.data.process_x(sample, self.x_vocabulary, self.settings))) for sample in x]
+        return self.model.predict([translator.data.process_x(sample, self.x_vocabulary, self.settings) for sample in x])
 
     def predict_raw(self, *args, **kwargs):
         return [translator.data.convert_raw(y, self.y_vocabulary, self.settings)
@@ -60,11 +62,13 @@ if __name__ == '__main__':
             "decoder_cells": 3,
             "optimizer": "rmsprop",
             "loss": "categorical_crossentropy",
-            "batch_size": 1,
-            "epochs": 3,
+            "batch_size": 32,
             "case_sensitive": False,
             "vocabulary_cutoff": 50000,
-            "metrics": [
-            ]
-        })
-    m.fit(raw_x, raw_y)
+            "metrics": None })
+    raw_validation_x = translator.data.load_corpus("../datasets/raw/validation_ger_x.txt")
+    raw_validation_y = translator.data.load_corpus("../datasets/raw/validation_ger_y.txt")
+    m.fit(raw_x, raw_y, validation_x=raw_validation_x, validation_y=raw_validation_y, epochs=30,
+            callbacks=[tf.keras.callbacks.ModelCheckpoint("../models/weights-{epoch:02d}-{val_loss:.3f}.hdf5"),
+                        tf.keras.callbacks.EarlyStopping(min_delta=0.0001, patience=2),
+                        tf.keras.callbacks.ReduceLROnPlateau(), tf.keras.callbacks.TensorBoard("tensorboard/", write_graph=True)])
